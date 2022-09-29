a3276993d5c9648236a34faebf9ab3868be5d07f
elisabethfarfan <elisabethfarfan@gmail.com>
2022-09-29 13:08:11 -0500

    estilos del componente register

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 
 export const Register = () => {
   return (
-    <div id ="" className="containerCenter ">
-        <h1>Register</h1>
-        <form>
-            <input  type="text" placeholder="Nombre" />
-            <input  type="email" placeholder="Correo"/>
-            <input  type="password" placeholder="Contraseña"/>
-            <input  type="submit" value="Registrarse"/>
-        </form>
+    <div className="containerGeneral containerCenter">
+      <div className="containerRegister containerCenter">
+        <img src=''/>
+        <h1>Regístrate en chatOwl</h1>
+          <form className="boxForm containerCenter">
+              <input className="input"  type="text" placeholder="Nombre" />
+              <input  className="input" type="email" placeholder="Correo"/>
+              <input  className="input" type="password" placeholder="Contraseña"/>
+              <input className="button input"  type="submit" value="Registrarse"/>
+          </form>
+          <p>Si ya tienes una cuenta, inicia sesión <strong>Aqui</strong></p>
+      </div>
+        
     </div>
   )
 }
