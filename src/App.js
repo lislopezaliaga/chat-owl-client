ad471faf96f2db14fda9de64afe27c422254b79a
elisabethfarfan <elisabethfarfan@gmail.com>
2022-09-30 19:11:33 -0500

    template de register, login y home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
+import { Home } from "./Components/Home";
 import { Login } from "./Components/Login";
 import { Register } from './Components/Register';
 
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Register/>}/>
         <Route exact path="/register" element={<Register/>}/>
-        <Route exact path="/login" element={<Login/>}/>   
+        <Route exact path="/login" element={<Login/>}/> 
+        <Route exact path="/home" element={<Home/>}/>  
       </Routes>
   </Router>
   );
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import logo from '../images/logo.png'
+import React from 'react';
+import logo from '../images/logo.png';
+import buhoAvion from '../images/buhoAvion.png';
+
 
 export const Register = () => {
   return (
-    <div className="containerGeneral containerCenter">
-      <div className="containerRegister containerCenter">
-        <img className="logoOwl" src={logo}/>
-        <h1>Regístrate en chatOwl</h1>
-          <form className="boxForm containerCenter">
+    <div className="generalContainer">
+
+      <div className='leftContainer'>
+        <img className="imgBuhoAvion" src={buhoAvion}/>
+      </div>
+
+      <div className="containerRegister">
+        <img className="imgLogo" src={logo}/>
+        <h1>ChatOwl</h1>
+          <form className="boxForm">
               <input className="input"  type="text" placeholder="Nombre" />
               <input  className="input" type="email" placeholder="Correo"/>
               <input  className="input" type="password" placeholder="Contraseña"/>
-              <input className="button input"  type="submit" value="Registrarse"/>
+              <input className="button"  type="submit" value="Registrarse"/>
           </form>
           <p>Si ya tienes una cuenta, inicia sesión <strong>Aquí</strong></p>
+
       </div>
         
     </div>
