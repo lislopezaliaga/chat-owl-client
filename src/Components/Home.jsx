c068f2d04a9c204da5bdb7dac6e51732ce291f95
elisabethfarfan <elisabethfarfan@gmail.com>
2022-10-18 16:04:20 -0500

    estilos perfil del cliente

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,6 +4,7 @@ import exit from '../images/exit.png';
 import userAvatarn from '../images/user.png';
 import { Chanel } from './Chanel';
 import { Chats } from './Chats';
+import { PerfilUsuario } from './PerfilUsuario';
 import { Users } from './Users';
 
 export const Home = () => {
@@ -23,11 +24,7 @@ export const Home = () => {
           <h2 className='titleChat'>ChatOwl</h2>
         </div>
         <div className='boxMenu'>
-          <div className='boxUser'>
-            <p className='nameUser'>{dataUser.name}</p>
-            <img className="avatar" alt='imágen de un avatar' src={userAvatarn} />
-
-          </div>
+        
           <img className="cerrarSesion" alt='imágen de cerrarSesion' src={exit} />
         </div>
       </div>
@@ -35,7 +32,10 @@ export const Home = () => {
       <div className='generalBoxBodyHome'>
        <Chanel  setChanelUnique={setChanelUnique}></Chanel>
        <Chats chanelUnique={chanelUnique} setChanelUnique={setChanelUnique} ></Chats>
-       <Users></Users>
+       <div className='boxUsersConected'>
+          <PerfilUsuario />
+          <Users></Users>
+       </div>
       </div>
       <div className='footer'>
 
